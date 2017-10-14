4b93151001905fb0a85687040df7d158e28091a0
Sanpreet Singh <32572322+sanpreet-ml@users.noreply.github.com>
2017-10-14 14:57:33 +0530

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -1,4 +1,5 @@
-for i in range(0,5):
+#Very informative code and one must learn this so that basix knowledge of inner loop is fetched.
+for i in range(0,5):                        
    for j in range(0,i+1):
-       print("*",end="")
+       print("*",end="")                   #Please see whethet end="" works in python 2.6, if not please upgrade it tp 3.x version
    print("\r")
